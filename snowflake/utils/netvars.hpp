f6a0dc3938f76e8381f0dd494595e1d10c702c16
AtiPLS <atihajd@gmail.com>
2018-04-15 12:28:07 +0200

    New NetVar manager.
    A re-implementation of the netvar manager that (hopefully) dumps every netvar.

--- snowflake/utils/netvars.hpp
+++ snowflake/utils/netvars.hpp
@@ -8,65 +8,70 @@
 
 class netvar_manager : public singleton<netvar_manager> {
 private:
-	std::map<unsigned int, recv_prop*> netvars_map;
-	void dump_table_recursive( recv_table* table ) {
-		for ( auto i = 0; i < table->props_count; ++i ) {
-			auto prop = &table->props [ i ];
+	std::unordered_map<unsigned int, recv_prop*> var_map;
 
-			if ( !prop || isdigit( prop->prop_name [ 0 ] ) )
-				continue;
-			if ( strcmp( prop->prop_name, "baseclass" ) == 0 )
-				continue;
+	void dump_table(recv_table* table) {
+		if (table->props_count < 0)
+			return;
 
-			if ( prop->prop_type == send_prop_type::_data_table && prop->data_table != nullptr && prop->data_table->table_name [ 0 ] == 'D' ) {
-				dump_table_recursive( prop->data_table );
-			}
+		for (auto i = 0; i < table->props_count; ++i) {
+			recv_prop *prop = &table->props[i];
+			if (!prop)
+				continue;
 
 			std::string hash_string = table->table_name;
-			hash_string.append( "->" );
-			hash_string.append( prop->prop_name );
+			hash_string.append("->");
+			hash_string.append(prop->prop_name);
 
-			netvars_map [ fnv_hash( hash_string.c_str( ) ) ] = prop;
+			auto hash = fnv_hash(hash_string.data());
+
+			if (var_map.find(hash) == var_map.end())
+				var_map.insert({ hash, prop });
+
+			if (prop->data_table)
+				this->dump_table(prop->data_table);
+
 		}
 	}
-
 public:
-	void initialize( client_class* client_data ) {
-		for ( auto data = client_data; data; data = data->next_ptr ) {
-			if ( data->recvtable_ptr ) {
-				dump_table_recursive( data->recvtable_ptr );
+
+	void initialize(client_class* client_data) {
+		for (auto data = client_data; data; data = data->next_ptr) {
+			if (data->recvtable_ptr) {
+				this->dump_table(data->recvtable_ptr);
 			}
 		}
 	}
 
-	unsigned short get_offset( unsigned int hash ) {
-		if ( !netvars_map [ hash ] ) return 0;
-		return netvars_map [ hash ]->offset;
+	unsigned short get_offset(unsigned int hash) {
+		auto i = var_map.find(hash);
+		if (i != var_map.end())
+			return var_map[hash]->offset;
 	}
-	recv_prop* get_prop( unsigned int hash ) {
-		return netvars_map [ hash ];
+
+	recv_prop* get_prop(unsigned int hash) {
+		auto i = var_map.find(hash);
+		if (i != var_map.end())
+			return var_map[hash];
 	}
 };
 
 #define offset_fn(type, var, offset) \
-	type& var() { \
-		return *(type*)(uintptr_t(this) + offset); \
-	} \
-
+		type& var() { \
+			return *(type*)(uintptr_t(this) + offset); \
+		} 
 #define netvar_fn(type, var, hash) \
-	type& var() { \
-		static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
-		return *(type*)(uintptr_t(this) + _offset); \
-	} \
-
+		type& var() { \
+			static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
+			return *(type*)(uintptr_t(this) + _offset); \
+		} 
 #define netvar_ptr_fn(type, var, hash) \
-	type* var() { \
-		static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
-		return (type*)(uintptr_t(this) + _offset); \
-	} \
-
+		type* var() { \
+			static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
+			return (type*)(uintptr_t(this) + _offset); \
+		} 
 #define netvar_offset_fn(type, var, hash, offset) \
-	type& var() { \
-		static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
-		return *(type*)(uintptr_t(this) + _offset + offset); \
-	} \
+		type& var() { \
+			static auto _offset = netvar_manager::get().get_offset(fnv_hash(hash)); \
+			return *(type*)(uintptr_t(this) + _offset + offset); \
+		} 
